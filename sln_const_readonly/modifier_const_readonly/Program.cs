0b92714a4856dc57f627a8da0713d483e2f713fd
vancisin peter <vancisin.peter@gmail.com>
2017-10-16 12:00:56 +0200

    code file

---
+++ sln_const_readonly/modifier_const_readonly/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modifier_const_readonly
+{
+    public class  Modifiers
+    {
+        public const double interestRate = 5.25;
+        public readonly int mortgageLength = 12;
+
+        public Modifiers(int length)
+        {
+            this.mortgageLength = length;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(interestRate);
+            Console.WriteLine(Modifiers.interestRate);
+            Modifiers instance = new Modifiers(23);
+            Console.WriteLine(instance.mortgageLength);
+
+            
+
+            
+
+        }
+    }
+}
